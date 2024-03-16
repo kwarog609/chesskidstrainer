601665b4c9bb87cc76a17a0f00c11471a62b6b24
kwarog609 <alexsiblagan@gmail.com>
2024-03-17 01:32:21 +0800

    fixed bugs and fixed commented issue

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -24,26 +24,12 @@ import { Piece } from '../../models/pieces';
 export class BoardComponent implements OnInit,AfterViewInit   {
   public board:Board;
   public empty_board;
-  // public board1: Array<Square> = [];
   public size: number = 64;
   public board_dim: string = '400px';
-  // public rank: number = 8;
   public is_legal: boolean = true;
   public player_turn: string = "white";
-  // public file: Array<string> = ['a','b','c','d','e','f','g','h',];
-  // public initial_piece:{[key: string]: string} = {a:'rook',b:'knight',c:'bishop',d:'queen',e:'king',f:'bishop',g:'knight',h:'rook',}
-  // public square: Square[] = [];
   public current_drag:any;
-  // public board_index: Array<any> = [
-  //   ['a8','b8','c8','d8','e8','f8','g8','h8',],
-  //   ['a7','b7','c7','d7','e7','f7','g7','h7',],
-  //   ['a6','b6','c6','d6','e6','f6','g6','h6',],
-  //   ['a5','b5','c5','d5','e5','f5','g5','h5',],
-  //   ['a4','b4','c4','d4','e4','f4','g4','h4',],
-  //   ['a3','b3','c3','d3','e3','f3','g3','h3',],
-  //   ['a2','b2','c2','d2','e2','f2','g2','h2',],
-  //   ['a1','b1','c1','d1','e1','f1','g1','h1',]
-  // ]
+
   public game_notations: Array<string> = [];
   public game_notation_number: number = 1;
 
@@ -99,18 +85,11 @@ export class BoardComponent implements OnInit,AfterViewInit   {
     if ( this.board.check_legal(this.current_drag, move) == true && this.current_drag.piece.your_turn) {
       console.log("check legal returned true")
       var count: number;
-      // console.log(cell.piece)
-      // console.log(this.board1[index])
-      // console.log(this.current_drag.cell_name)
-      // console.log(this.current_drag.piece)
-      // to drop place = from current drag
+
       this.player_turn = (this.player_turn == 'black') ? "white":"black";
       const index = parseInt(String(move.cell_id)) - 1;
       console.log("using the parameter move: " + move.cell_name + " piece: " + move.piece.piece)
       
-      // this.board1[index].piece = this.current_drag.piece
-      // this.board1[index].piece_url = this.current_drag.piece_url 
-      // this.board1[index].piece_color = this.current_drag.piece_color
       console.log("this is the index: " + index + "this is the destination square: " + this.board.board[index].cell_name)
       this.board.board[index].piece = this.current_drag.piece;
       this.board.board[index].piece.first_move = false;
@@ -138,11 +117,8 @@ export class BoardComponent implements OnInit,AfterViewInit   {
       // then set null the from place
       this.current_drag.piece = new Piece('','no color')
 
-      // this.current_drag.piece.url= ''  
       console.log(this.board.board[index])
-    // } else {
-    //   console.log("illegal move");
-    // 
+
     }
   } catch(error){
     console.log("error: " + error)
--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -22,17 +22,4 @@ export class CellComponent {
 
   }
 
-  // ngOnInit(
-  //    cell_id: Number = 20,
-  //    cell_name: String = 'e1',
-  //    color: String ='black',
-  //    is_occupied: Boolean = false,
-  //    size: String = '100px',
-  // ){
-  //   this.cell_id = cell_id;
-  //   this.cell_name = cell_name;
-  //   this.color = color;
-  //   this.is_occupied = is_occupied;
-  //   this.size = size;
-  // }
 }
